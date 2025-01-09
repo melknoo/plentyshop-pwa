2eb3ff65c4cceb8e1645f5776b153a674a038c2f
melknoo <melvinknop@gmx.de>
2025-01-09 11:31:14 +0100

    wip

--- apps/web/components/ui/MediaCard/MediaCard.vue
+++ apps/web/components/ui/MediaCard/MediaCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-if="showComponent" :class="['flex flex-col md:flex-row items-center', positionClass]">
+  <div v-if="showComponent" class="sms-mediacard--img" :class="['flex flex-col md:flex-row items-center', positionClass]">
     <div v-if="image && image.trim() !== ''" :class="['w-full', 'md:w-1/2', { 'mb-4': text }]">
-      <img :src="image" :alt="alt" width="728" height="485" class="w-full h-auto object-cover" />
+      <img :src="image" :alt="alt" width="728" height="485" class="w-full h-auto object-cover " />
     </div>
 
     <div v-if="text && text.trim() !== ''" :class="['w-full', textWidthClass, textAlignmentClass]" v-html="text"></div>
