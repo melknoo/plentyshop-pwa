3075592e0d8bbed158835e576a8529dd80135289
melknoo <melvinknop@gmx.de>
2025-01-27 09:23:42 +0100

    wip

--- apps/web/components/HnHero/HnHero.vue
+++ apps/web/components/HnHero/HnHero.vue
@@ -1,3 +1,26 @@
 <template>
-    <p></p>
+    <div class="sms-hero--wrapper">
+        <div class="sms-hero--row">
+            <img class="sms-hero--image" src="https://suckmystraw.com/wp-content/themes/suckmystraw-theme/img/SMS-Logo-onBlackGlow.svg">
+        </div>
+        <div class="sms-hero--row">
+            <button class="sms-button--primary mx-auto sms-d--block">zum shop</button>
+        </div>
+        <div class="sms-hero--row">
+            <div class="sms-hero--textwrapper">
+                <div class="sms-hero--container">
+                    <h2 class="text-white sms-hero--h2">Der härteste Papierstrohhalm</h2>
+                    <div class="sms-hero--sepwrap">
+                        <div class="sms-hero--seperator"></div>
+                    </div>
+                    <h2 class="text-white sms-hero--h2">Hält länger als dein Drink</h2>
+                    <div class="sms-hero--sepwrap">
+                        <div class="sms-hero--seperator"></div>
+                    </div>
+                    <h2 class="text-white sms-hero--h2">Umweltfreundliche
+                    Alternative zu Plastik</h2>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
