d122ce0c575ee84c499feb6f25056d2b49169c9f
melknoo <melvinknop@gmx.de>
2025-01-27 13:33:32 +0100

    text image done

--- apps/web/components/HnTextImage/HnTextImage.vue
+++ apps/web/components/HnTextImage/HnTextImage.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="sms-textimage--wrapper">
-        <div class="sms-textimage--row">
+        <div class="sms-textimage--row relative overflow-hidden">
             <div class="sms-textimage--textwrapper">
                 <h2 class="sms-textimage--headline typography-display-3 uppercase">Sehr <br> sehr <br> stabiler <br> strohhalm</h2>
                 <div class="sms-textimage--inner">
                     <p class="sms-textimage--copytext sms-copytext sms-mb--2">Macht definitiv nicht schlapp, egal wie feucht es wird.<br>
     
-    Zwei Jahre Forschung und Training haben unsere Papierstrohhalme ausgesprochen belastbar gemacht. Lebensmitteltauglich und komplett frei von bedenklichen Farben und Klebern, stehen sie in jedem Getränk wie eine verführerische Eins und freuen sich auf jede Suck-Action.</p>
+                    Zwei Jahre Forschung und Training haben unsere Papierstrohhalme ausgesprochen belastbar gemacht. Lebensmitteltauglich und komplett frei von bedenklichen Farben und Klebern, stehen sie in jedem Getränk wie eine verführerische Eins und freuen sich auf jede Suck-Action.</p>
                 <div class="sms-textimage--iconcontainer my-5">
                     <div class="sms-textimage--iconcontainer-icon--1 sms-textimage--iconcontainer-icon"></div>
                     <p class="sms-copytext sms-pl--4">Standfest bis zum Ende</p>
@@ -21,9 +21,11 @@
                 </div>
                 </div>
             </div>
+            <div class="sms-asset--straw-3 sms-textimage--straw"></div>
+            <div class="sms-asset--straw-4 sms-textimage--straw"></div>
         </div>
-        <div class="sms-textimage--row">
-            <div class="sms-seperator"></div>
+        <div class="sms-textimage--row relative">
+            <div class="sms-seperator sms-textimage--seperator"></div>
         </div>
     </div>
 </template>
