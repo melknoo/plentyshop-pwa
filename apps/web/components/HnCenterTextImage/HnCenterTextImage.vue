2f90b6ff50ce33a37dccf43ff4217a9452aa5c8a
melknoo <melvinknop@gmx.de>
2025-01-29 08:41:54 +0100

    added new element

---
+++ apps/web/components/HnCenterTextImage/HnCenterTextImage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="sms-centerimage--wrapper">
+        <div class="sms-textimage--row relative overflow-hidden">
+            <div class="sms-centerimage--textwrapper">
+                <h2 class="sms-centerimage--headline typography-display-3 uppercase">Es kommt eben doch<br> aufs
+                    aussehen an.</h2>
+                <div class="sms-centerimage--strawwrapper">
+                    <img class="sms-centerimage--strawimage" src="https://suckmystraw.com/wp-content/themes/suckmystraw-theme/img/straws/straw-h-blue.png">
+                </div>
+                <div class="sms-centerimage--inner text-left">
+                    <p class="sms-textimage--copytext sms-copytext sms-mb--2">Wähle jetzt aus 3000 Farben und
+                        unterschiedlichen Dicken und Längen deine individuellen Strohhalme aus. Unsere Favorite Suckers
+                        gibt’s natürlich auch in Neon, Metallic und Pastell. Auf Wunsch bekommst du zudem
+                        individualisierte Trinkhalme mit eigenem Logo auf jedem Halm und / oder auf der Verpackung.
+                        Zeitnah und schnell bis vor die Haustür.</p>
+                    <div class="sms-centerimage--iconcontainer my-5">
+                        <div class="sms-centerimage--iconcontainer-icon--1 sms-centerimage--iconcontainer-icon"></div>
+                        <p class="sms-copytext sms-pl--4">Eigenes Logo</p>
+                    </div>
+                    <div class="sms-centerimage--iconcontainer my-5">
+                        <div class="sms-centerimage--iconcontainer-icon--2 sms-centerimage--iconcontainer-icon"></div>
+                        <p class="sms-copytext sms-pl--4">Passt genau zu deinen <br>Vorstellungen</p>
+                    </div>
+                    <img class="sms-centerimage--niceimage" src="https://suckmystraw.com/wp-content/uploads/2020/12/nice@4x.png">
+                </div>
+            </div>
+        </div>
+        <div class="sms-textimage--row relative">
+            <div class="sms-seperator sms-textimage--seperator"></div>
+        </div>
+    </div>
+</template>
