4ceffcac7dfcd5ac46748a983eeead1313361916
melknoo <melvinknop@gmx.de>
2025-02-05 11:38:27 +0100

    wip

--- apps/web/components/HnCta/HnCta.vue
+++ apps/web/components/HnCta/HnCta.vue
@@ -1,6 +1,6 @@
 <template>
     <div :class="text?.wrapper_class" class="sms-cta--wrapper">
-        <div class="sms-cta--row flex">
+        <div class="sms-cta--row flex sms-mobile--column">
             <div :class="images?.img1_wrapper_class" class="sms-cta--imgwrapper">
                 <img :class="images?.img1_class" class="sms-cta--image" :src="images?.img1_src">
                 <img :class="images?.img2_class" class="sms-cta--image" :src="images?.img2_src">
