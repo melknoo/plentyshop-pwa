8badcbe6262b43b744969e74fbf35f2bf37ebf4a
melknoo <melvinknop@gmx.de>
2025-06-18 14:41:04 +0200

    wip

--- apps/web/components/CheckoutPayment/CheckoutPayment.vue
+++ apps/web/components/CheckoutPayment/CheckoutPayment.vue
@@ -22,7 +22,7 @@
           }"
           class="h-20 flex flex-col items-center justify-center py-4 px-4 cursor-pointer rounded-md border 
           border-neutral-200 -outline-offset-2 hover:border-primary-50 hover:bg-primary-50 
-          active:border-primary-100 active:bg-primary-50 peer-checked:outline peer-checked:outline-2 peer-checked:outline-pink-500
+          active:border-primary-100 active:bg-primary-50 peer-checked:outline peer-checked:outline-2 peer-checked:outline-white peer-checked:bg-pink
           peer-disabled:opacity-50 peer-disabled:bg-neutral-100 peer-disabled:border-neutral-200 peer-disabled:cursor-not-allowed peer-disabled:[&_img]:grayscale"
         >
           <span v-if="paymentMethod.id === -1">
@@ -30,7 +30,7 @@
             <span class="font-medium">{{ t('checkoutPayment.creditCard') }}</span>
           </span>
           <NuxtImg v-else :src="paymentMethod.icon" :alt="paymentMethod.name" class="!h-[40px]" loading="lazy" />
-          <span class="text-xs mt-2 text-neutral-500">{{ paymentMethod.name }}</span>
+          <span class="text-xs font-semibold mt-2 text-white">{{ paymentMethod.name }}</span>
         </span>
       </label>
     </div>
