5a6954dce774ec1bb30b8b40f488dc4fd0b8680e
melknoo <melvinknop@gmx.de>
2025-08-18 11:44:38 +0200

    fixes

--- apps/web/components/ui/ProductCard/ProductCard.vue
+++ apps/web/components/ui/ProductCard/ProductCard.vue
@@ -41,11 +41,11 @@
       </slot>
     </div>
     <div class="p-2 uppercase border-t border-neutral-200 sms-border--pink typography-text-sm flex flex-col flex-auto">
-      <SfLink :tag="NuxtLink" :to="productPath" class="no-underline" variant="secondary" data-testid="productcard-name">
+      <SfLink :tag="NuxtLink" :to="productPath" class="min-h-[40px] no-underline" variant="secondary" data-testid="productcard-name">
         {{ name }}
       </SfLink>
       <p class="normal-case typography-text-sm " data-testid="product-name">
-              <span v-if="hasOneOrZeroVariations" >{{ product.variation.name }}</span>
+              <!-- <span v-if="hasOneOrZeroVariations" >{{ product.variation.name }}</span> -->
               {{ product.variation.externalId }}
       </p>
       <div class="flex items-center pt-1 gap-1" :class="{ 'mb-2': !productGetters.getShortDescription(product) }">
